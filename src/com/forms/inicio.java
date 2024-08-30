988ec69844759b0b395af1ea3d7a15c511a65059
CarlosTrev <alu.21130838@correo.itlalaguna.edu.mx>
2024-08-29 20:34:23 -0600

    init commit

--- src/com/forms/inicio.java
+++ src/com/forms/inicio.java
@@ -16,7 +16,7 @@ public class inicio {
                 JOptionPane.showMessageDialog(null, txtNombre.getText());
             }
         });
-
+        //Carlos Adrian
     }
 
     public static void main(String[] args) {
