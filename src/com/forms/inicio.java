5bb0bb6dfc4f54f1471d7fd9150fa1e06121553e
CarlosTrev <alu.21130838@correo.itlalaguna.edu.mx>
2024-08-30 15:14:06 -0600

    Agregue mi numero de control

--- src/com/forms/inicio.java
+++ src/com/forms/inicio.java
@@ -16,7 +16,7 @@ public class inicio {
                 JOptionPane.showMessageDialog(null, txtNombre.getText());
             }
         });
-        //Carlos Adrian Treviño Martinez
+        //Carlos Adrian Treviño Martinez 21130838
     }
 
     public static void main(String[] args) {
